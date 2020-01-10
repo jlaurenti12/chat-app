f97e29d6f1ce7225c96ae5292040a72118f82a54
Jacob Laurenti <jake.laurenti@gmail.com>
2020-01-09 20:02:12 -0500

    Added Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBF1Cw9Hr3f_U0_YKLO09GqA2QCd8t-36A",
+  authDomain: "chat-app-2020.firebaseapp.com",
+  databaseURL: "https://chat-app-2020.firebaseio.com",
+  projectId: "chat-app-2020",
+  storageBucket: "chat-app-2020.appspot.com",
+  messagingSenderId: "379822062942",
+  appId: "1:379822062942:web:94d9856bbdc0cd3d3fa3cf"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 function App() {
   return (
