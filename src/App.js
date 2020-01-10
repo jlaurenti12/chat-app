6b2fa3b4c31176053bd6aae7409ec39428926edb
Jacob Laurenti <jake.laurenti@gmail.com>
2020-01-09 22:13:00 -0500

    room lists displaying

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }  from 'react';
+import RoomList from './components/RoomList.js';
 import * as firebase from 'firebase';
 
-// Your web app's Firebase configuration
 var firebaseConfig = {
   apiKey: "AIzaSyBF1Cw9Hr3f_U0_YKLO09GqA2QCd8t-36A",
   authDomain: "chat-app-2020.firebaseapp.com",
@@ -13,28 +11,24 @@ var firebaseConfig = {
   messagingSenderId: "379822062942",
   appId: "1:379822062942:web:94d9856bbdc0cd3d3fa3cf"
 };
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+   constructor(props) {
+     super(props);
+  }
+
+
+  render() {
+   return (
+     <div className="App">
+      <RoomList firebase={firebase}/> 
+     </div>
+    );
+  }
+
 }
+
 
 export default App;
